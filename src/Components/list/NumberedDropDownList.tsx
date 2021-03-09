d9243a68ec6bc68c8bbfa9fe17ea2b9d2eea114c
CMB <thaichibao@gmail.com>
2021-03-09 13:26:55 +0700

    rm ..DropDown..tsx files (rename to ..Dropdown..tsx)

--- src/Components/list/NumberedDropDownList.tsx
+++
@@ -1,9 +0,0 @@
-import DropdownList, { DropdownListProps } from "./DropdownList";
-
-const NumberedDropdownList: React.FC<DropdownListProps> = (props) => {
-  return (<DropdownList {...props} ChildrenContainer={Ol} />)
-};
-export default NumberedDropdownList;
-
-const Ol: React.FC = ({ children }) => <ol className="ml-5">{children}</ol>
-
