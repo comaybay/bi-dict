ac41ce742b43d6c292ef4dad7e985f14c4aecaca
CMB <thaichibao@gmail.com>
2021-04-18 22:00:08 +0700

    remove sendRequest's azure url (disabled)

--- src/utils/sendRequest.ts
+++ src/utils/sendRequest.ts
@@ -1,5 +1,4 @@
-// const API_URL = "https://localhost:44313/api/";
-const API_URL = "https://bi-dict-api20210206231826.azurewebsites.net/api/";
+const API_URL = "https://localhost:44313/api/";
 export default function sendRequest(path: string) {
   return fetch(`${API_URL}${path}`);
 }
